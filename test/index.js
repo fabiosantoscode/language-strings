ebd5a6c2287c37b3bf28f3ad6d2381596eabce60
Fábio Santos <fabiosantosart@gmail.com>
2017-06-24 14:15:10 +0100

    first commit

---
+++ test/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const languageStrings = require('..')
+const assert = require('assert')
+
+describe('require("language-strings")', () => {
+  it('escapes HTML', () => {
+    assert.equal(
+      languageStrings.html`<div class="${'<b>'}">${'hello<'}`,
+      '<div class="&lt;b&gt;">hello&lt;'
+    )
+  })
+  it('escapes bash', () => {
+    assert.equal(
+      languageStrings.shell`echo foo-${'bar baz'}`,
+      `echo foo-'bar baz'`
+    )
+  })
+  it('escapes CSS', () => {
+    assert.equal(
+      languageStrings.css`* { background: url(${'http://www.placecage.com/200/300'}); }`,
+      String.raw`* { background: url(http\:\/\/www\.placecage\.com\/200\/300); }`
+    )
+  })
+})
